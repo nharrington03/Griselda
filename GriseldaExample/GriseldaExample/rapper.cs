2c89ce39922c349b49661ef3b719bea6412302be
nharrington03 <55925356+nharrington03@users.noreply.github.com>
2020-02-04 18:58:29 -0500

    first checkin
    comment

---
+++ GriseldaExample/GriseldaExample/rapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GriseldaExample
+{
+    class rapper
+    {
+       public Boolean haveBars;
+       public Boolean canSing;
+
+        public string Rap ()
+        {
+            return "";
+        }
+    }
+}
